540e63948c0a095ae5da936b75a5a2c3bc261f8e
danilobenda <danilobenda@gmail.com>
2026-05-12 10:43:05 +0200

    Initial Commit

---
+++ Fuel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fuel
+//
+//  Created by Danilo Benda on 12/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fuel/FuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuelApp.swift
+//  Fuel
+//
+//  Created by Danilo Benda on 12/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
